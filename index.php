e4b8e3c6a8a815caf9cf4319a0be1fcb0cbad800
Marc Català <reskit@gmail.com>
2012-11-30 11:11:03 +0100

    Eliminats els arxius javascripts i posat el seu codi inline

--- index.php
+++ index.php
@@ -279,7 +279,34 @@ $doc->addScript($this->baseurl.'/templates/'.$this->template.'/javascript/md_sty
 
 			<jdoc:include type="modules" name="debug" />
 		<?php endif; ?>
-		<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/javascript/campus.js"></script>
-		<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/javascript/analytics.js"></script>
+		<script type="text/javascript">
+            var a = new Request({
+	            method: 'post',
+	            url: '/campus/local/loggedinas.php',
+	            onSuccess: function(responseText, responseXML){
+		            if (responseText){
+			            if (responseText.length > 22){
+				            responseText = responseText.substr(0,22) + '...';
+			            }
+		                $("frmAccess").set("class", "hidden");
+		                var myname = new Element('a', {
+			                href: '/campus',
+			                title: 'Entra al campus',
+		                    html: 'Heu entrat com:<br/>'+ responseText,
+		                    class: 'usercampus'
+		                }).inject($("access"));
+		            }
+	            }
+            }).send();
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36486089-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
 	</body>
 </html>
