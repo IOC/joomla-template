3465bcff9f30682603b0592d235913abfebc5aa9
Marc Català <mcatala@itteria.cat>
2018-03-13 14:11:09 +0100

    New changes

--- html/com_search/search/default.php
+++ html/com_search/search/default.php
@@ -3,23 +3,34 @@
  * @package     Joomla.Site
  * @subpackage  com_search
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
+
 JHtml::_('formbehavior.chosen', 'select');
+
 ?>
-<div class="fluid-container page-header default ">
+<div class="fluid-container page-header default">
     <div class="container">
         <h1><?php echo $this->escape($this->params->get('page_title')); ?></h1>
     </div>
 </div>
-<div class="item-page-default search<?php echo $this->pageclass_sfx; ?> container">
-<?php echo $this->loadTemplate('form'); ?>
-<?php if ($this->error == null && count($this->results) > 0) :
-    echo $this->loadTemplate('results');
-else :
-    echo $this->loadTemplate('error');
-endif; ?>
+<div class="item-page-default search<?php echo $this->pageclass_sfx; ?>">
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <h1 class="page-title">
+            <?php if ($this->escape($this->params->get('page_heading'))) : ?>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+            <?php else : ?>
+                <?php echo JText::_('TPL_IOC_SEARCH'); ?>
+            <?php endif; ?>
+        </h1>
+    <?php endif; ?>
+    <?php echo $this->loadTemplate('form'); ?>
+    <?php if ($this->error == null && count($this->results) > 0) : ?>
+        <?php echo $this->loadTemplate('results'); ?>
+    <?php else : ?>
+        <?php echo $this->loadTemplate('error'); ?>
+    <?php endif; ?>
 </div>
--- html/com_search/search/default_results.php
+++ html/com_search/search/default_results.php
@@ -3,43 +3,44 @@
  * @package     Joomla.Site
  * @subpackage  com_search
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
+
 ?>
-
-<dl class="search-results<?php echo $this->pageclass_sfx; ?>">
+<div class="search-results<?php echo $this->pageclass_sfx; ?>">
 <?php foreach ($this->results as $result) : ?>
-    <dt class="result-title">
-        <?php echo $this->pagination->limitstart + $result->count . '. ';?>
-        <?php if ($result->href) :?>
-            <a href="<?php echo JRoute::_($result->href); ?>"<?php if ($result->browsernav == 1) :?> target="_blank"<?php endif;?>>
-                <?php echo $this->escape($result->title);?>
-            </a>
-        <?php else:?>
-            <?php echo $this->escape($result->title);?>
+    <div class="result-data">
+        <?php if ($result->href) : ?>
+            <a href="<?php echo JRoute::_($result->href); ?>"<?php if ($result->browsernav == 1) : ?> target="_blank"<?php endif; ?>>
+                <?php // $result->title should not be escaped in this case, as it may ?>
+                <?php // contain span HTML tags wrapping the searched terms, if present ?>
+                <?php // in the title. ?>
+                <span class="result-title">
+                    <?php echo $result->title; ?>
+                </span>
+        <?php else : ?>
+            <?php // see above comment: do not escape $result->title ?>
+            <?php echo $result->title; ?>
         <?php endif; ?>
-    </dt>
-    <?php if ($result->section) : ?>
-        <dd class="result-category">
-            <span class="small<?php echo $this->pageclass_sfx; ?>">
-                (<?php echo $this->escape($result->section); ?>)
-            </span>
-        </dd>
-    <?php endif; ?>
-    <dd class="result-text">
+    <div class="result-text">
         <?php echo $result->text; ?>
-    </dd>
-    <?php if ($this->params->get('show_date')) : ?>
-        <dd class="result-created<?php echo $this->pageclass_sfx; ?>">
-            <?php echo JText::sprintf('JGLOBAL_CREATED_DATE_ON', $result->created); ?>
-        </dd>
+    </div>
+    <?php if ($result->section) : ?>
+        <div class="result-category">
+            <span class="small<?php echo $this->pageclass_sfx; ?>">
+                <?php echo $this->escape($result->section); ?>
+            </span>
+        </div>
     <?php endif; ?>
+    <?php if ($result->href) : ?>
+        </a>
+    <?php endif; ?>
+    </div>
 <?php endforeach; ?>
-</dl>
-
+</div>
 <div class="content-pagination">
     <?php echo $this->pagination->getPagesLinks(); ?>
 </div>
