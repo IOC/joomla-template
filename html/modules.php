9eaad9077c7b289a6acf2cdecb72880dde5a840c
Marc Català <mcatala@itteria.cat>
2018-04-11 15:55:41 +0200

    Override layout

--- html/modules.php
+++ html/modules.php
@@ -26,7 +26,7 @@ function modChrome_block($module, &$params, &$attribs)
 {
  	if (!empty ($module->content)) : ?>
            <div class="block <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
-           	<div class="moduletable">           	
+            <div class="moduletable">
 	           	<?php if ($module->showtitle != 0) : ?>
 			<div class="module-title">
 	                		<h3 class="title"><span class="<?php echo $params->get('header_class'); ?>" ></span><?php echo $module->title ; ?></h3>
@@ -35,7 +35,7 @@ function modChrome_block($module, &$params, &$attribs)
 	                	<div class="module-content">
 	                		<?php echo $module->content; ?>
 	                	</div>
-              </div>             	
+              </div>
            </div>
 	<?php endif;
 }
@@ -43,7 +43,7 @@ function modChrome_block($module, &$params, &$attribs)
 		if (!empty ($module->content)) :
 		?>
            <div class="MBstyle <?php  if ($params->get('moduleclass_sfx')!='') : ?><?php  echo $params->get('moduleclass_sfx'); ?><?php  endif; ?>">
-           	<div class="moduletable">           	
+            <div class="moduletable">
 	           	<?php  if ($module->showtitle != 0) : ?>
 			<div class="module-title">
 	                		<h3 class="title"><span class="<?php echo $params->get('header_class'); ?>" ></span><?php echo $module->title ; ?></h3>
@@ -53,9 +53,146 @@ function modChrome_block($module, &$params, &$attribs)
 	                	<div class="module-content">
 	                		<?php  echo $module->content; ?>
 	                	</div>
-              </div>             	
+              </div>
            </div>
-	<?php 
+	<?php
 		endif;
 	}
-?>
+
+function modChrome_studies($module, &$params, &$attribs)
+{
+    if (!empty ($module->content)) : ?>
+        <div class="block <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
+            <div class="studies">
+                <?php if ($module->showtitle != 0) : ?>
+                    <div class="title">
+                        <div>
+                            <h1><span class="<?php echo $params->get('header_class'); ?>" ></span><?php echo $module->title ; ?></h1>
+                        </div>
+                    </div>
+                <?php endif; ?>
+                <div class="studies-content">
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+}
+
+function modChrome_sub_studies($module, &$params, &$attribs)
+{
+    if (!empty ($module->content)) : ?>
+        <div class="fluid-container substudies <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
+            <div class="substudies-content">
+                <?php if ($module->showtitle != 0) : ?>
+                    <div class="title">
+                        <h1><span class="<?php echo $params->get('header_class'); ?>" ></span><?php echo $module->title ; ?></h1>
+                    </div>
+                <?php endif; ?>
+                <div class="substudies-elements">
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+}
+
+function modChrome_menu_footer($module, &$params, &$attribs)
+{
+    if (!empty ($module->content)) : ?>
+        <div class="top-col">
+            <div class="hidden-xs hidden-sm block <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
+                <div class="studies">
+                    <?php if ($module->showtitle != 0) : ?>
+                        <div class="title">
+                            <h3><?php echo $module->title ; ?></h3>
+                        </div>
+                    <?php endif; ?>
+                    <div class="studies-content">
+                        <?php echo $module->content; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="panel-group visible-xs visible-sm">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#footer-collapse" href="#footer-collapse-<?php echo $module->id; ?>" aria-expanded="false"><?php echo $module->title ; ?><span class="custom-icon plus"></span></a>
+                    </h3>
+                </div>
+                <div id="footer-collapse-<?php echo $module->id; ?>" class="panel-collapse collapse">
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+}
+
+function modChrome_banner_footer($module, &$params, &$attribs)
+{
+    if (!empty ($module->content)) : ?>
+        <div class="top-col">
+            <div class="hidden-xs hidden-sm block <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
+                <div class="moduletable">
+                    <?php if ($module->showtitle != 0) : ?>
+                        <div class="title">
+                            <h3><?php echo $module->title ; ?></h3>
+                        </div>
+                    <?php endif; ?>
+                    <div class="banner-content">
+                        <?php echo $module->content; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="panel-group panel-mobile visible-xs visible-sm">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#footer-collapse" href="#footer-collapse-<?php echo $module->id; ?>" aria-expanded="false"><?php echo $module->title ; ?><span class="custom-icon plus"></span></a>
+                    </h3>
+                </div>
+                <div id="footer-collapse-<?php echo $module->id; ?>" class="panel-collapse collapse">
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+}
+
+function modChrome_employment($module, &$params, &$attribs)
+{
+    if (!empty ($module->content)) : ?>
+        <div class="block <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
+            <div class="row">
+                <?php if ($module->showtitle != 0) : ?>
+                    <div class="title">
+                        <h1><span class="<?php echo $params->get('header_class'); ?>" ></span><?php echo $module->title ; ?></h1>
+                    </div>
+                <?php endif; ?>
+                <div class="employment-content">
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+}
+
+function modChrome_sub_menu($module, &$params, &$attribs)
+{
+    if (!empty ($module->content)) : ?>
+        <div class="fluid-container submenu <?php if ($params->get('moduleclass_sfx')!='') : ?><?php echo $params->get('moduleclass_sfx'); ?><?php endif; ?>">
+            <div class="submenu-content">
+                <?php if ($module->showtitle != 0) : ?>
+                    <div class="title">
+                        <h1><span class="<?php echo $params->get('header_class'); ?>" ></span><?php echo $module->title ; ?></h1>
+                    </div>
+                <?php endif; ?>
+                <div>
+                    <?php echo $module->content; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+}
